d26f8c71b289fcdb314ebde9a426cd95aa1a3110
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-20 13:11:58 +0530

    Login Signup page js part pending

--- recipe-finder/src/Pages/Allroutes.jsx
+++ recipe-finder/src/Pages/Allroutes.jsx
@@ -3,12 +3,16 @@ import {Route,Routes} from "react-router-dom"
 import Home from './Home'
 import Recipes from './Recipes'
 import Contact from './Contact'
+import Login from './Login'
+import Signup from './Singnup'
 
 const Allroutes = () => {
   return (
     <div>
       <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<Signup/>}/>
           <Route path='/recipe' element={<Recipes/>}/>
           <Route path='/contact' element={<Contact/>}/>
       </Routes>
--- recipe-finder/src/Pages/Singnup.jsx
+++ recipe-finder/src/Pages/Singnup.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
 
 
-const Login = () => (
+const Signup = () => (
   <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
     <Grid.Column style={{ maxWidth: 450 }}>
       <Header as='h2' color='black' textAlign='center'>
-         Log-in to your account
+         Create your account
       </Header>
       <Form size='large'>
         <Segment stacked>
+        <Form.Input
+            fluid
+            icon='user'
+            iconPosition='left'
+            placeholder='Nmane'
+            type='Name'
+          />
           <Form.Input fluid icon='at' iconPosition='left' placeholder='E-mail address' />
           <Form.Input
             fluid
@@ -18,17 +25,16 @@ const Login = () => (
             placeholder='Password'
             type='password'
           />
-
           <Button color='green' fluid size='large'>
-            Login
+            Signup
           </Button>
         </Segment>
       </Form>
       <Message>
-        New to us? <a href='./signup' className='link'>Sign Up</a>
+        Already have an account? <a href='./login' className='link'>Login</a>
       </Message>
     </Grid.Column>
   </Grid>
 )
 
-export default Login
+export default Signup
